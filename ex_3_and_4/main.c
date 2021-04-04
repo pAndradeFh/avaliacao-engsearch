c8ccc3f778e3d95a1369dd09f13a01b65529231b
Paulo Andrade <p.andrade.fh@gmail.com>
2021-04-04 18:29:53 -0300

    added solve of exercise 3

--- ex_3_and_4/main.c
+++ ex_3_and_4/main.c
@@ -1,18 +1,52 @@
 #include <stdio.h>
-#include <string.h>
 
-void ConcatRemove(char s, char t, char k)
+int ConcatRemove(char *s, char *t, int k)
 {
-    int length = strlen(s);
-    printf("Length of the string = %d\n", length);
+    int i = 0, size_s = 0, size_t = 0;
+    while (s[i] != '\0' && t[i] != '\0' && s[i] == t[i])
+    {
+        i++;
+        size_s++;
+        size_t++;
+    }
+
+    while (s[size_s] != '\0')
+    {
+        size_s++;
+    }
+
+    while (t[size_t] != '\0')
+    {
+        size_t++;
+    }
+
+    int min_s = size_s - i; //string to be removed from the source
+    int min_t = size_t - i; //string to be appended on the source end
+
+    // printf("%i %i %i %i",min_s, min_t, i, k - (min_s + min_t));
+
+    if (k < min_s + min_t)
+    { //case the number of steps cannot even cover the removing/appending on the source end
+        return 0;
+    }
+    else
+    {
+        // otherwise, we check if we can keep removing/addind one element till we exauste our steps
+        // In this case, we need k - (min_s + min_t) to be an even number
+        if ((k - (min_s + min_t)) % 2 == 0)
+            return 1;
+        else
+            return 0;
+    }
 }
 
-int main(args)
+int main(int args)
 {
     char s[100], t[100];
-    int k;
-    scanf("%s", s);
-    scanf("%s", t);
+    int k, result;
+    scanf("%s", s); //starting
+    scanf("%s", t); //desire
     scanf("%i", &k);
-    ConcatRemove(s, t, k);
+    result = ConcatRemove(s, t, k);
+    printf(result == 0 ? "false" : "true");
 }
